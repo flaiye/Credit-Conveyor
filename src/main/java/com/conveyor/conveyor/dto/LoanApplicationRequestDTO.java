974cac2c6bac72177b93f357df02246807a190c5
flaiye <flaiye@gmail.com>
2022-11-29 08:57:54 +0300

    added spring-boot-starter-validation dependency, added some validation constarints to DTO

--- src/main/java/com/conveyor/conveyor/dto/LoanApplicationRequestDTO.java
+++ src/main/java/com/conveyor/conveyor/dto/LoanApplicationRequestDTO.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -13,10 +16,19 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 public class LoanApplicationRequestDTO {
+    @Min(10000)
     private BigDecimal amount;
     private Integer term;
+    @Min(2)
+    @Max(30)
+    @NotBlank
     private String firstName;
+    @Min(2)
+    @Max(30)
+    @NotBlank
     private String lastName;
+    @Min(2)
+    @Max(30)
     private String middleName;
     private String email;
     private LocalDate birthdate; // or birthDate ?
